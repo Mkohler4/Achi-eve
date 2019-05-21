697f25dddc644e0f81483a5b4ead3068fe9e02b4
Mkohler4 <Markb19n19>
2019-05-21 03:28:29 -0400

    updated landing page added hamburger menu features and horizontal scroll bar features

--- lib/landingPage.dart
+++ lib/landingPage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 class LandingPage extends StatefulWidget {
+  bool isSelected1;
+  bool isSelected2;
+  LandingPage({this.isSelected1 = false, this.isSelected2 = false});
   @override
   _LandingPageState createState() => _LandingPageState();
 }
@@ -12,17 +15,13 @@ class _LandingPageState extends State<LandingPage> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-          titleSpacing: 50,
+          iconTheme: new IconThemeData(color: Colors.black),
           title: Text("Projects", style: TextStyle(color: Colors.black)),
           centerTitle: true,
           backgroundColor: Colors.transparent,
           elevation: 0,
-          leading: Icon(
-            Icons.menu,
-            color: Colors.black,
-          ),
           bottom: TabBar(
-            labelPadding: EdgeInsets.fromLTRB(100, 0, 100, 0),
+            labelPadding: EdgeInsets.fromLTRB(52, 0, 52, 0),
             isScrollable: true,
             tabs: [
               Tab(
@@ -45,6 +44,45 @@ class _LandingPageState extends State<LandingPage> {
                           fontSize: 20))),
             ],
           ),
+        ),
+        drawer: Drawer(
+            child: ListView(
+          children: <Widget>[
+            ListTile(
+              selected: true,
+              title: Text(
+                "Website",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {},
+            ),
+            ListTile(
+              selected: false,
+              title: Text(
+                "Appliciation",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {},
+            ),
+          ],
+        )),
+        body: TabBarView(
+          children: [
+            Icon(
+              Icons.directions_car,
+              color: Colors.black,
+            ),
+            Icon(
+              Icons.directions_transit,
+              color: Colors.black,
+            ),
+            Icon(
+              Icons.directions_bike,
+              color: Colors.black,
+            ),
+          ],
         ),
       ),
     );
