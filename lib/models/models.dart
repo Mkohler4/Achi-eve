43990ed2fd3a6709be43c00f40445336c047b3dd
Ibtesam Mahmood <ibtesam.mahmood2@gmail.com>
2019-05-21 02:01:57 -0400

    graph finished

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,5 +1,7 @@
 
 
+import 'dart:math';
+
 class Profile{
   String _name;
 
@@ -10,8 +12,61 @@ class Profile{
 
 class Project{
 
+
+  List<Component> ownerList(String name){
+    List<Component> list = [];
+
+
+    return list;
+  }
+
+  List<Component> openList(){
+    List<Component> list = [];
+
+
+    return list;
+  }
+
+  List<Component> closedList(){
+    List<Component> list = [];
+
+
+    return list;
+  }
+
+  List<Component> takenList(){
+    List<Component> list = [];
+
+
+    return list;
+  }
+
 }
 
 class Component{
 
+}
+
+class Graph{
+  List<List<bool>> _adj;
+
+  List<Component> _components;
+
+  void addNode(Component component){
+    _components.add(component);
+    for (List<bool> list in _adj) {
+      list.add(false);
+    }
+    _adj.add(List<bool>(_adj[0].length));
+  }
+
+  void addConnection(int startIndex, int endIndex){
+    _adj[startIndex][endIndex] = true;
+  }
+
+  bool isConnection(int startIndex, int endIndex) => _adj[startIndex][endIndex];
+
+  Component getComponent(int index){
+    return _components[index];
+  }
 }
